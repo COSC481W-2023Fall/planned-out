f1a508c88b00b6d12be2ecb21e9e85fe43957ee3
tedgravlin <theodoregravlin@pm.me>
2023-10-09 14:49:33 -0400

    feat: Implement UI for tasks

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -2,6 +2,24 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
+import Form from "react-bootstrap/Form";
+
+function CheckBox({ taskname, id }) {
+    return (
+        <ListGroup.Item className="task">
+            {<Form.Check type='checkbox' id={id} label={taskname} />}
+        </ListGroup.Item>
+    );
+}
+
+const tasksList = [
+    'Take the dog for a walk',
+    'Clean the kitchen',
+    'Get groceries',
+    'Make dinner',
+    'Complete homework for class'
+]
 
 const Home = () => {
 
@@ -12,6 +30,13 @@ const Home = () => {
                     <Col>
                         <Card className="tasks-card">
                             <Card.Title>Tasks</Card.Title>
+                            <ListGroup className="task-list">
+                                {/* For loop for each task in the tasks list */}
+                                {tasksList
+                                    .map((task) => (
+                                        <CheckBox className="task" id={"task" + tasksList.indexOf(task)} taskname={task}></CheckBox>
+                                    ))}
+                            </ListGroup>
                         </Card>
                     </Col>
                     <Col sm={8}>
