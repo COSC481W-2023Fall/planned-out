67063e1f818f032cab289d9cb53c2afff24d545b
khammerm <khammermeister44@gmail.com>
2023-10-11 17:44:22 -0400

    feat: created database connection file
    *fix: server.mjs now imports db from connection file
    *fix: successful connection to mongodb after created connection file

---
+++ back-end/db/conn.mjs
@@ -0,0 +1,19 @@
+import { MongoClient } from "mongodb";
+
+const username = encodeURIComponent("admin");
+const password = encodeURIComponent("SZIESYi6rteN7wjN")
+let uri = 'mongodb+srv://admin:SZIESYi6rteN7wjN@planned-out-database.g8zc5cf.mongodb.net/?retryWrites=true&w=majority'
+
+const client = new MongoClient(uri)
+
+let conn;
+try{
+  conn = await client.connect();
+  
+  console.log("MongoDB Connection Successful.")
+}catch(e){
+  console.error(e)
+}
+
+let db = conn.db("Tasks")
+export default db;
