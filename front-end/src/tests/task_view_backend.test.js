0075a7002f8f0b1b2d6fcf769c76f05433872324
Matt <mbraun3@emich.edu>
2023-10-22 09:41:55 -0400

    fix: update more request urls after merging

---
+++ front-end/src/tests/task_view_backend.test.js
@@ -0,0 +1,5 @@
+test("checks to see if a database connection is made", () => {
+
+  expect(console.log("MongoDB Connection Successful."));
+});
+// This tests to see if a database connection has been made
