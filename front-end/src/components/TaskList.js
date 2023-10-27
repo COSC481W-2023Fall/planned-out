41c5d800654c584714918d1b043e247ef595df47
Kyle Moorhead <kmoorhea@emich.edu>
2023-10-27 08:39:11 -0400

    refactor: optional link from testing to production
    refactor: optional link from testing to production
    
    * refactor: optional link to production for tasklist

--- front-end/src/components/TaskList.js
+++ front-end/src/components/TaskList.js
@@ -3,8 +3,8 @@ import InputGroup from "react-bootstrap/InputGroup";
 import Form from "react-bootstrap/Form";
 import { useState, useEffect } from "react";
 
-let link = "http://localhost:5050/"
-//link = "https://planned-out-backend-jdx6.onrender.com/"
+//let link = "http://localhost:5050/"
+let link = "https://planned-out-backend-jdx6.onrender.com/"
 
 function TaskList() {
 
