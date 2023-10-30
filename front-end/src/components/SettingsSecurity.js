2da6d9389f9f8c012aced56f08032f5df3233678
tedgravlin <theodoregravlin@pm.me>
2023-10-30 13:28:48 -0400

    feat: Create components for each settings page
    feat: Add components for each settings page
    
    * Added a separate component for each settings page: Security, Profile, Appearance, Privacy

---
+++ front-end/src/components/SettingsSecurity.js
@@ -0,0 +1,15 @@
+import Card from "react-bootstrap/Card";
+
+const SettingsCard = (props) => {
+
+    return (
+        <>
+            <Card className="settings-card">
+                <Card.Title>Security</Card.Title>
+            </Card>
+        </>
+    )
+
+}
+
+export default SettingsCard;
