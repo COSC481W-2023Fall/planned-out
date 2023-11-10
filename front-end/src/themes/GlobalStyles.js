dcf73b2b18d8bcf0b78c12b69376c0c3caab450b
tedgravlin <theodoregravlin@pm.me>
2023-11-10 14:38:32 -0500

    feat: Automatically change theme based on system theme
    feat: Automatically change theme based on system theme
    
    * Added prefers-color-scheme query to CSS
    * Switched to using css variables for most colors

--- front-end/src/themes/GlobalStyles.js
+++ front-end/src/themes/GlobalStyles.js
@@ -1,9 +1,14 @@
 import { createGlobalStyle} from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
-  .btn, .btn.active {
+  .btn, .btn.active, .btn:hover {
     background: ${({ theme }) => theme.colors.accent};
     border-color: ${({ theme }) => theme.colors.accent};
+  }
+
+  div.nav-item .btn.active, div.nav-item .btn:hover {
+    background: ${({ theme }) => theme.colors.accent} !important;
+    border-color: ${({ theme }) => theme.colors.accent} !important;
   }
 
   div.react-calendar button {
