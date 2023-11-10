03ce3910ad041874a0c4732de1e5ee4d03521203
bluebronze <2013novaknadya@gmail.com>
2023-11-09 19:39:37 -0500

    Realigned the Form.labels
    Makes it look nicer

--- front-end/src/components/SettingsSecurity.js
+++ front-end/src/components/SettingsSecurity.js
@@ -12,28 +12,28 @@ const SettingsCard = (props) => {
                 <Card.Subtitle>Change Password</Card.Subtitle>
                 <Card.Body>
                     
-                    <Form>
-                        <Form.Group controlId="SecEmail">
+                    <Form style={{ textAlign: 'left' }}>
+                        <Form.Group controlId="SecEmail" className="mb-3">
                             <Form.Label>Email address</Form.Label>
                             <Form.Control type = "email" placeholder="something@example.com"/>
                         </Form.Group>
 
-                        <Form.Group controlId="SecPassOld">
-                            <Form.Label>Current Password</Form.Label>
+                        <Form.Group controlId="SecPassOld" className="mb-3">
+                            <Form.Label >Current Password</Form.Label>
                             <Form.Control type = "password" placeholder="Enter your current password" />
                         </Form.Group>
 
-                        <Form.Group controlId="SecPassNew1">
+                        <Form.Group controlId="SecPassNew1" className="mb-3">
                             <Form.Label>New Password</Form.Label>
                             <Form.Control type = "password" placeholder="Enter a new password" />
                         </Form.Group>
 
-                        <Form.Group controlId="SecPassNew2">
+                        <Form.Group controlId="SecPassNew2" className="mb-3">
                             <Form.Label>Confirm New Password</Form.Label>
                             <Form.Control type = "password" placeholder="Repeat new password" />
                         </Form.Group>
 
-                        <Button className="login-button" variant="primary" type="submit" >
+                        <Button className="login-button" variant="primary" type="submit">
                              Submit
                         </Button>
 
