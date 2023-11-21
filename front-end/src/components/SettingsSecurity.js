d840435dc716ce5a39647791c2ef3a160ad6e38d
Kyle Moorhead <Moorhead212@gmail.com>
2023-11-21 11:22:21 -0500

    fix: security settings data validation
    fix: security settings data validation
    
    * add: checks from registration page (edited to match security page) to verify password is allowed. Checks for length, matching, not contain spaces, and not null.

--- front-end/src/components/SettingsSecurity.js
+++ front-end/src/components/SettingsSecurity.js
@@ -1,7 +1,7 @@
 import Card from "react-bootstrap/Card";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 let link = localStorage.getItem("backendURL");
 
@@ -11,6 +11,7 @@ const SettingsCard = (props) => {
     const [newPassword1, setNewPassword1] = useState("");
     const [newPassword2, setNewPassword2] = useState("");
     const [errors, setErrors] = useState(false);
+    const [isFormValid, setIsFormValid] = useState(false);
 
     const validation = (e) => {
         const { name, value } = e.target;
@@ -19,23 +20,72 @@ const SettingsCard = (props) => {
                 ...prevErrors,
                 [name]: `Required`,
             }));
+        } else if (name === "newPassword1" && (value.length < 8 || value.length > 20)) {
+            setErrors((prevErrors) => ({
+                ...prevErrors,
+                newPassword1: "Password must be at least 8 characters long",
+            }));
+        } else if ((name === "newPassword1" && value.includes(" ")) || (name === "newPassword2" && value.includes(" "))) {
+            setErrors((prevErrors) => ({
+                ...prevErrors,
+                newPassword1: "Password may not contain spaces",
+            }));
+        } else if (
+            name === "newPassword1" &&
+            value.length <= 20 &&
+            value.length >= 8 &&
+            !value.includes(" ") &&
+            !value.includes("")
+        ) {
+            setErrors((prevErrors) => ({
+                ...prevErrors,
+                newPassword1: "",
+            }));
+        } else if (
+            (name === "newPassword2" && value === newPassword1) ||
+            (name === "newPassword1" && value === newPassword2)
+        ) {
+            setErrors((prevErrors) => ({
+                ...prevErrors,
+                newPassword1: "",
+                newPassword2: "",
+            }));
+            document.getElementById("badLogin").innerHTML =
+                "";;
+        } else if (
+            (name === "newPassword2" && value !== newPassword1) ||
+            (name === "newPassword1" && value !== newPassword2)
+        ) {
+            setErrors((prevErrors) => ({
+                ...prevErrors,
+                newPassword2: "Passwords do not match",
+            }));
+            document.getElementById("badLogin").innerHTML =
+                "Passwords do not match!";
         } else {
             setErrors((prevErrors) => ({
                 ...prevErrors,
                 [name]: "",
-            }));
-        }
-
-        if (newPassword1 !== newPassword2) {
-            e.preventDefault();
+            }))
             document.getElementById("badLogin").innerHTML =
-                "Passwords do not match!";
-        }
-        else {
-            document.getElementById("badLogin").innerHTML =
-                "";
+                "";;
         }
     };
+
+    useEffect(() => {
+        const isAllValid =
+            username.trim() !== "" &&
+            password.trim() !== "" &&
+            newPassword1.trim() !== "" &&
+            newPassword2.trim() !== "" &&
+            newPassword1.value === newPassword2.value &&
+            !errors.username &&
+            !errors.password &&
+            !errors.newPassword1 &&
+            !errors.newPassword2;
+
+        setIsFormValid(isAllValid);
+    }, [username, password, newPassword1, newPassword2, errors]);
 
     const handleSubmit = (e) => {
         console.log("Attempting Login");
@@ -45,9 +95,13 @@ const SettingsCard = (props) => {
         if (
             !errors.username &&
             !errors.password &&
+            !errors.newPassword1 &&
+            !errors.newPassword2 &&
             username !== "" &&
             password !== "" &&
-            newPassword1 === newPassword2
+            newPassword1 !== "" &&
+            newPassword2 !== "" &&
+            newPassword1.value === newPassword2.value
         ) {
             fetch(link + "update", {
                 method: "PUT",
@@ -104,27 +158,35 @@ const SettingsCard = (props) => {
 
                         <Form.Group controlId="SecPassNew1" className="mb-3">
                             <Form.Label>New Password</Form.Label>
-                            <Form.Control type="password" placeholder="Enter a new password" onChange={(e) => {
+                            <Form.Control name="newPassword1" type="password" placeholder="Enter a new password" onChange={(e) => {
                                 setNewPassword1(e.target.value);
+                                validation(e)
                             }}
-                                isInvalid={!!errors.password} onBlur={(e) => validation(e)} />
+                                isInvalid={!!errors.newPassword1} onBlur={(e) => validation(e)} />
+                            <Form.Control.Feedback type="invalid">
+                                {errors.newPassword1}
+                            </Form.Control.Feedback>
                         </Form.Group>
+
 
                         <Form.Group controlId="SecPassNew2" className="mb-3">
                             <Form.Label>Confirm New Password</Form.Label>
-                            <Form.Control type="password" placeholder="Repeat new password" onChange={(e) => {
+                            <Form.Control name="newPassword2" type="password" placeholder="Repeat new password" onChange={(e) => {
                                 setNewPassword2(e.target.value);
                                 validation(e);
                             }}
-                                isInvalid={!!errors.password} onBlur={(e) => validation(e)} />
+                                isInvalid={!!errors.newPassword2} onBlur={(e) => validation(e)} />
+                            <Form.Control.Feedback type="invalid">
+                                {errors.newPassword2}
+                            </Form.Control.Feedback>
                         </Form.Group>
                         <Form.Text id="badLogin"></Form.Text>
                         <br />
                         {/* This is a temporary fix to center the button because Kyle doesn't know what he's doing. - See Settings.css for styling */}
                         <div class="button-div">
-                        <Button className="settings-change-button" variant="primary" onClick={handleSubmit} type="submit" >
-                            Submit
-                        </Button>
+                            <Button className="settings-change-button" variant="primary" onClick={handleSubmit} disabled={!isFormValid} type="submit" >
+                                Submit
+                            </Button>
                         </div>
                     </Form>
                 </Card.Body>
