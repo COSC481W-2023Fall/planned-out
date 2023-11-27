160527949b23442e72bf64263fddb97b70d5334d
Matt Braun <mbraun3@emich.edu>
2023-11-27 14:48:11 -0500

    Style: Update styling for clarity *Begin implementation of new logo *Made register link slightly larger *rename 'home' to 'tasks' *remove stats and friends button *remove navbar from login page

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -8,29 +8,29 @@ import "./index.css";
 import Settings from "./pages/Settings.js";
 import Login from "./pages/Login.js";
 import Registration from "./pages/Registration.js";
-import * as themes from './themes/schema.json';
-import { setToLS } from './utils/storage';
+import * as themes from "./themes/schema.json";
+import { setToLS } from "./utils/storage";
 
 export default function App() {
-    setToLS('all-themes', themes.default);
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<Home />} />
-                </Route>
-                <Route path="/login" element={<Layout />}>
-                    <Route index element={<Login />} />
-                </Route>
-                <Route path="/settings" element={<Layout />}>
-                    <Route index element={<Settings />} />
-                </Route>
-                <Route path="/registration" element={<Layout />}>
-                    <Route index element={<Registration />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    );
+  setToLS("all-themes", themes.default);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+        <Route path="/login" element={<Login />}>
+          {/* <Route index element={<Login />} /> */}
+        </Route>
+        <Route path="/settings" element={<Layout />}>
+          <Route index element={<Settings />} />
+        </Route>
+        <Route path="/registration" element={<Layout />}>
+          <Route index element={<Registration />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- front-end/src/pages/Layout.js
+++ front-end/src/pages/Layout.js
@@ -4,16 +4,17 @@ import Navbar from "react-bootstrap/Navbar";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import { LinkContainer } from "react-router-bootstrap";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
+import "../fonts/orange juice 2.0.ttf";
 
 const Layout = () => {
-  // For testing purposes: Store the backend link in localStorage 
+  // For testing purposes: Store the backend link in localStorage
   // so that it can be pulled from any page. This makes it so that
   // we don't have to change the backend link on every single page
   // when testing things with localhost
   let renderBackend = "https://planned-out-backend-jdx6.onrender.com/";
-  let localBackend = "http://localhost:5050/"
+  let localBackend = "http://localhost:5050/";
 
   // Use render backend
   localStorage.setItem("backendURL", renderBackend);
@@ -21,7 +22,7 @@ const Layout = () => {
   //localStorage.setItem("backendURL", localBackend);
 
   const navigate = useNavigate();
-  const userCookie = localStorage.getItem('user');
+  const userCookie = localStorage.getItem("user");
   const [isUserLoggedIn, setLoggedIn] = useState([]);
   const [isUserLoggedOut, setLoggedOut] = useState([]);
 
@@ -32,16 +33,15 @@ const Layout = () => {
       if (window.location.pathname !== "/registration") {
         navigate(`/login`);
       }
-    }
-    else {
-      setLoggedIn(true)
+    } else {
+      setLoggedIn(true);
       setLoggedOut(false);
     }
   }, [navigate, userCookie]);
 
   // Log user out
   function logUserOut() {
-    localStorage.removeItem('user');
+    localStorage.removeItem("user");
     navigate(`/login`);
   }
 
@@ -49,29 +49,15 @@ const Layout = () => {
     <>
       <Navbar>
         <Container>
-          <Navbar.Brand href="/">
-            <img
-              src="/favicon-32x32.png"
-              width={"32px"}
-              height={"32px"}
-              alt={"Planned-Out Logo"}
-            />
-            Planned-Out
+          <Navbar.Brand className="logo" href="/">
+            Planned
+            <br />
+            Out
           </Navbar.Brand>
           <Nav className="me-auto" variant="pills">
             <Nav.Item>
               <LinkContainer to="/">
-                <Button className="main-nav-button">Home</Button>
-              </LinkContainer>
-            </Nav.Item>
-            <Nav.Item>
-              <LinkContainer to="/">
-                <Button className="main-nav-button">Stats</Button>
-              </LinkContainer>
-            </Nav.Item>
-            <Nav.Item>
-              <LinkContainer to="/">
-                <Button className="main-nav-button">Friends</Button>
+                <Button className="main-nav-button">Tasks</Button>
               </LinkContainer>
             </Nav.Item>
             <Nav.Item>
@@ -79,20 +65,22 @@ const Layout = () => {
                 <Button className="main-nav-button">Settings</Button>
               </LinkContainer>
             </Nav.Item>
-            {isUserLoggedOut &&
+            {isUserLoggedOut && (
               <Nav.Item>
                 <LinkContainer to="/login">
                   <Button className="main-nav-button">Login</Button>
                 </LinkContainer>
               </Nav.Item>
-            }
-            {isUserLoggedIn &&
-              < Nav.Item >
+            )}
+            {isUserLoggedIn && (
+              <Nav.Item>
                 <LinkContainer to="/login">
-                  <Button onClick={logUserOut} className="main-nav-button">Log Out</Button>
+                  <Button onClick={logUserOut} className="main-nav-button">
+                    Log Out
+                  </Button>
                 </LinkContainer>
               </Nav.Item>
-            }
+            )}
           </Nav>
         </Container>
       </Navbar>
