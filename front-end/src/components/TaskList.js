da942e0fa2566d7a2009903328f68666376be6dd
Kyle Moorhead <Moorhead212@gmail.com>
2023-11-28 10:08:34 -0500

    feat: view task description by clicking on task
    feat: view task description by clicking on task
    
    * add: modal for task description
    * style: underline label on hover to give indication to click
    * bug: clicking label reverts task status back to status from page load

--- front-end/src/components/TaskList.js
+++ front-end/src/components/TaskList.js
@@ -2,12 +2,17 @@ import ListGroup from "react-bootstrap/ListGroup";
 import InputGroup from "react-bootstrap/InputGroup";
 import Form from "react-bootstrap/Form";
 import { useState, useEffect } from "react";
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 let link = localStorage.getItem("backendURL");
 
 function TaskList({ username }) {
-    console.log(username);
+
     const [tasksList, setTaskList] = useState([]);
+    const [modalShow, setModalShow] = useState(false);
+    const [taskName, setTaskName] = useState([]);
+    const [taskDesc, setTaskDesc] = useState([]);
 
     useEffect(() => {
         // verify user info from URL to display task list
@@ -23,6 +28,26 @@ function TaskList({ username }) {
             .then((res) => res.json())
             .then((data) => setTaskList(data));
     }, [username]);
+
+    function MyVerticallyCenteredModal(props) {
+        return (
+            <Modal
+                {...props}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        Task Name: {taskName}
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    Description: {taskDesc}
+                </Modal.Body>
+            </Modal>
+        );
+    }
 
     function CheckBox({ id, taskID, taskName, taskDate, taskDesc, taskStatus }) {
         let labelID = id + "label";
@@ -45,14 +70,16 @@ function TaskList({ username }) {
                         type="checkbox"
                         id={checkedID}
                     />
-                    <label className={"checked"} id={labelID} htmlFor={id}>
+                    <label className={"checked"} id={labelID} htmlFor={id} onClick={() => handleModal(taskName, taskDesc)}>
                         {taskName}
                     </label>
                 </InputGroup>
             );
         }
-        return (
-            <InputGroup>
+        else {
+
+            return (
+                <InputGroup>
                 <Form.Check onChange={handleChecked(
                     id,
                     labelID,
@@ -61,15 +88,17 @@ function TaskList({ username }) {
                     taskDate,
                     taskDesc,
                     taskStatus
-                )}
+                    )}
                     type="checkbox"
                     id={id}
-                />
-                <label className={"unchecked"} id={labelID} htmlFor={id}>
+                    />
+                <label className={"unchecked"} id={labelID} htmlFor={id} onClick={() => handleModal(taskName, taskDesc)}>
                     {taskName}
                 </label>
+                
             </InputGroup>
         );
+    }
     }
 
     const handleChecked = (checkID, labelID, taskID, taskName, taskDate, taskDesc, taskStatus) =>
@@ -112,23 +141,40 @@ function TaskList({ username }) {
             console.log("Successfully updated task!");
         };
 
+    const handleModal = (taskName, taskDesc) => {
+        setModalShow(true);
+        setTaskDesc(taskDesc);
+        setTaskName(taskName);
+    };
+
     return (
-        <ListGroup data-testid="TaskListGroup" className="task-list no-scroll">
-            {/* For loop for each task in the tasks list */}
-            {tasksList.map((task) => (
-                <ListGroup.Item key={task._id} className="task">
-                    <CheckBox
-                        className="task"
-                        id={"task" + tasksList.indexOf(task)}
-                        taskID={task._id}
-                        taskName={task.taskName}
-                        taskDate={task.taskDate}
-                        taskDesc={task.taskDesc}
-                        taskStatus={task.taskStatus}
-                    ></CheckBox>
-                </ListGroup.Item>
-            ))}
-        </ListGroup>
+        <>
+            <ListGroup data-testid="TaskListGroup" className="task-list no-scroll">
+                {/* For loop for each task in the tasks list */}
+                {tasksList.map((task) => (
+                    <>
+
+                        <ListGroup.Item key={task._id} className="task">
+                            <CheckBox
+                                className="task"
+                                id={"task" + tasksList.indexOf(task)}
+                                taskID={task._id}
+                                taskName={task.taskName}
+                                taskDate={task.taskDate}
+                                taskDesc={task.taskDesc}
+                                taskStatus={task.taskStatus}
+                            >
+                            </CheckBox>
+
+                        </ListGroup.Item>
+                    </>
+                ))}
+
+            </ListGroup>
+            <MyVerticallyCenteredModal
+                show={modalShow}
+                onHide={() => setModalShow(false)} />
+        </>
     )
 }
 
